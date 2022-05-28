aa23d56e2ae32eb5d2242db279a1029e40a87b07
Bouchbout-sami <ks_bouchbout@esi.dz>
2022-05-28 17:14:44 +0100

    test

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , UploadFile , File
 import OCR
 from PIL import Image
+import io
 
 
 app = FastAPI()
+
+@app.post("/predict/")
+async def root(file: UploadFile = File(...)):
+  image =  await file.read()
+  image = Image.open(io.BytesIO(image))
+  return {"result":OCR.resultat_final(image)}
 
 @app.get("/corr")
 async def ocr(img_path : str):
