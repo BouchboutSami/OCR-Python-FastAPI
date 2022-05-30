d2a8dd726b0cd0f5dc71a1f7ca462a7c5c7e796e
Bouchbout-sami <ks_bouchbout@esi.dz>
2022-05-30 02:55:30 +0100

    image code 64

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import base64
 from fastapi import FastAPI , UploadFile , File
 import OCR
 from PIL import Image
@@ -14,8 +13,8 @@ async def root(file: UploadFile = File(...)):
   return {"result":OCR.resultat_final(image)}
 
 @app.get("/corr")
-async def ocr(imgcode : str):
-     test =Image.open(io.BytesIO(base64.decodebytes(bytes(imgcode, "utf-8"))))
+async def ocr(img_path : str):
+     test = Image.open(img_path)
      chiffre , score , font = OCR.resultat_final(test)
      return  {"result":chiffre,"score":score,"font":font}
 
